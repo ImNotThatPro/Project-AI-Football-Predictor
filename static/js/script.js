c04acb9d15c5d488c688c6954092129000fd82ff
ImNotThatPro <duonglonglangson@gmail.com>
2025-09-26 23:50:11 +0700

    Changing around with the ui

--- static/js/script.js
+++ static/js/script.js
@@ -6,11 +6,18 @@ async function getPrediction(){
     const result = await response.json();
     console.log(result)
     console.log(url)
-    document.getElementById('result').innerText = `Prediction: ${result.prediction} \n
+    document.getElementById('result').innerText = `\n Prediction: ${result.prediction} \n
                                                  Home team: ${teamA} Win chance : ${(result.probs['HomeWin'] * 100).toFixed(1)} % \n
                                                  Away team: ${teamB} Win chance : ${(result.probs['AwayWin'] * 100).toFixed(1)} % \n
                                                  Draw chance : ${(result.probs['Draw'] * 100).toFixed(1)} % \n
                                                  Model Accuracy ~ 52%` 
+    document.getElementById('form-block').style.display ='none';
+    document.getElementById('result-block').style.display ='block';
+
     console.log(result.probs)
 }
 
+async function goBack(){
+    document.getElementById('form-block').style.display ='block';
+    document.getElementById('result-block').style.display ='none';
+}
