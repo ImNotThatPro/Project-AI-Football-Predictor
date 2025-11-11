ee164d9a3a3c147aa20dbac6bef20a55a0f14dd1
ImNotThatPro <duonglonglangson@gmail.com>
2025-11-11 22:50:09 +0700

    Update testing model with CNN model

--- Model/testing.py
+++ Model/testing.py
@@ -1,18 +1,22 @@
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras import models, Input
 import matplotlib.pyplot as plt
 from keras.callbacks import TensorBoard
+import datetime
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train / 255.0
-X_test = X_test / 255.0
+X_train = X_train.reshape(-1, 28, 28, 1) / 255.0
+X_test = X_test.reshape(-1, 28, 28, 1) / 255.0
 
 model = Sequential([
-    Input(shape=(28, 28)),
+    Conv2D(32, (3,3), activation='relu', input_shape =(28,28,1)),
+    MaxPooling2D((2,2)),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D((2,2)),
     Flatten(),
-    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
     Dense(10, activation='softmax')
 ])
 
